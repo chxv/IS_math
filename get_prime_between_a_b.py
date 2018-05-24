681bf3356a9c8c120897a810cf9dd0eca1da66b7
xander <1149277941@qq.com>
2018-05-24 21:42:58 +0800

    使用筛法求[a，b]间所有素数

--- get_prime_between_a_b.py
+++ get_prime_between_a_b.py
@@ -10,14 +10,13 @@ def get_prime_num(a, b):
     # 筛法求素数
     if a > b:
         a, b = b, a
-
-    shai = [0 for _ in range(0, b+1)]
+    isPrime = [True for _ in range(0, b+1)]
     for i in range(2, b+1):
-        if shai[i] == 0:  # 如果当前i是素数
+        if isPrime[i]:  # 如果当前i是素数
             for j in range(i + i, b+1, i):  # 所有i的倍数都是合数
-                shai[j] = 1
-    a = 2 if a < 2 else a
-    return [i for i in range(a, b+1) if shai[i] == 0]
+                isPrime[j] = False
+    a = 2 if a < 2 else a  # 最少显示[2,b]范围的内容（a必须大于等于2）
+    return [i for i in range(a, b+1) if isPrime[i]]
 
 
 if __name__ == "__main__":
